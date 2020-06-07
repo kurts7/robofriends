a6e6d81928edca2c44cc8c9e5d6d4459e951696a
Ololosha <ololosha@Ololoshas-MacBook-Pro.local>
2020-06-08 01:54:53 +0300

    Updates

---
+++ precache-manifest.1a20c31a43a136733d64b26364835fcb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02b7f26cc2cf2e9d81acd01eba584116",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "badb2a3fa4b9dcd21df1",
+    "url": "/robofriends/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a3206089bab05f8a0aff",
+    "url": "/robofriends/static/js/2.1317703d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.1317703d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "badb2a3fa4b9dcd21df1",
+    "url": "/robofriends/static/js/main.0ffb1345.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/robofriends/static/media/logo.5d5d9eef.svg"
+  }
+]);
